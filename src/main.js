ccaca99a9ee6dc86087b8c5265ad4b7fe13b5288
yinbingfan <31506572+yinbingfan@users.noreply.github.com>
2019-02-12 17:05:02 +0800

    国际化

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import routes from './router/router'
 import FastClick from 'fastclick'
-
+import VueI18n from 'vue-i18n'
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function() {
         FastClick.attach(document.body);
@@ -21,9 +21,25 @@ document.addEventListener('touchstart', function(event) {
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+
 const router = new VueRouter({
     routes
 })
+Vue.use(VueI18n)
+
+export default ({ app, store }) => {
+    app.i18n = new VueI18n({
+        locale: store.state.locale,
+        fallbackLocale: store.state.locale,
+        messages: {
+            // 'en': require('~/locales/en.json')
+            // 'ja': require('~/locales/ja.json'),
+            // 'ko': require('~/locales/ko.json'),
+            // 'zh': require('~/locales/zh.json')
+        }
+    })
+
+}
 new Vue({
     router,
   render: h => h(App),
